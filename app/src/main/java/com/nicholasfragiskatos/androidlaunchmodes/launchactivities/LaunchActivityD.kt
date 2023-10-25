c274a79be321e61e6dcde2cacbe2d299a0998aa4
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-10-25 04:39:12 -0700

    - added skeletons of all example activities - turned on viewbinding

---
+++ app/src/main/java/com/nicholasfragiskatos/androidlaunchmodes/launchactivities/LaunchActivityD.kt
@@ -0,0 +1,12 @@
+package com.nicholasfragiskatos.androidlaunchmodes.launchactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nicholasfragiskatos.androidlaunchmodes.R
+
+class LaunchActivityD : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch_d)
+    }
+}
