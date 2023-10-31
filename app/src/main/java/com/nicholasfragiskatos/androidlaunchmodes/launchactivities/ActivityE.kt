0c2e9e015b4b8770cdddb796bcfe5d78a44ad107
Nicholas Fragiskatos <nicholas.fragiskatos@pm.me>
2023-10-31 08:08:41 -0700

    - formatting

--- app/src/main/java/com/nicholasfragiskatos/androidlaunchmodes/launchactivities/ActivityE.kt
+++ app/src/main/java/com/nicholasfragiskatos/androidlaunchmodes/launchactivities/ActivityE.kt
@@ -1,15 +1,14 @@
 package com.nicholasfragiskatos.androidlaunchmodes.launchactivities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.nicholasfragiskatos.androidlaunchmodes.R
+import androidx.appcompat.app.AppCompatActivity
 import com.nicholasfragiskatos.androidlaunchmodes.databinding.ActivityEBinding
 
 class ActivityE : AppCompatActivity() {
 
-    private lateinit var binding : ActivityEBinding
+    private lateinit var binding: ActivityEBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEBinding.inflate(layoutInflater)
@@ -42,6 +41,10 @@ class ActivityE : AppCompatActivity() {
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
-        Toast.makeText(this, "onNewIntent called for Activity E", Toast.LENGTH_LONG).show()
+        Toast.makeText(
+            this,
+            "onNewIntent called for Activity E",
+            Toast.LENGTH_LONG,
+        ).show()
     }
 }
